111cab51a70a6720dd11d04a008b34c7cfc00c07
fpancald <fpancald@nd.edu>
2017-01-23 12:25:25 -0500

    add all files from fpancald working repository

---
+++ include/b1_timing.hpp
@@ -0,0 +1,16 @@
+
+#ifndef BERTINI1_TIMING_HPP
+#define BERTINI1_TIMING_HPP
+#include <cstddef>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+
+#include"bertini2/eigen_extensions.hpp"
+// #include"bertini2/mpfr_extensions.hpp"
+// #include"bertini2/mpfr_complex.hpp"
+
+#include "bertini_headers.hpp"
+
+
+#endif
